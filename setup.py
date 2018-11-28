b4e5628c82115669e66d5f586c0586bdd28fe6c6
ilnurgi <ilnurgi@mail.ru>
2018-11-28 06:44:48 +0300

    0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='django_nginx_access',
-    version='0.0.3',
+    version='0.1.1',
     author='Ильнур Гайфутдинов',
     author_email='ilnurgi87@gmail.com',
     description='Парсер nginx логов',
